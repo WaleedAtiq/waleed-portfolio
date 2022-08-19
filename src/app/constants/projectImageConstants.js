aa7189c560e97ee3a1bce03238e2aed2668efd77
Muneeb Hussain Modi <muneebhussainmodi@gmail.com>
2022-08-19 17:18:43 +0500

    Very big error resolved by senior react developer

--- src/app/constants/projectImageConstants.js
+++ src/app/constants/projectImageConstants.js
@@ -1,15 +1,52 @@
 const projectImages = {
-  snapteam: require.context(`../../assets/images/projectImages/snapteam`, false, /.*\.png$/),
-  pulse: require.context(`../../assets/images/projectImages/pulse`, false, /.*\.png$/),
-  measure: require.context(`../../assets/images/projectImages/measure`, false, /.*\.png$/),
-  wakency: require.context(`../../assets/images/projectImages/wakency`, false, /.*\.png$/),
-  benefactory: require.context(`../../assets/images/projectImages/benefactory`, false, /.*\.png$/),
-  lighthouse: require.context(`../../assets/images/projectImages/lighthouse`, false, /.*\.png$/),
-  nykaa: require.context(`../../assets/images/projectImages/nykaa`, false, /.*\.png$/),
-  vc_music_player: require.context(`../../assets/images/projectImages/vc_music_player`, false, /.*\.png$/)
+  snapteam: require.context(
+    `../../assets/images/projectImages/snapteam`,
+    false,
+    /.*\.png$/
+  ),
+  pulse: require.context(
+    `../../assets/images/projectImages/pulse`,
+    false,
+    /.*\.png$/
+  ),
+  measure: require.context(
+    `../../assets/images/projectImages/measure`,
+    false,
+    /.*\.png$/
+  ),
+  wakency: require.context(
+    `../../assets/images/projectImages/wakency`,
+    false,
+    /.*\.png$/
+  ),
+  benefactory: require.context(
+    `../../assets/images/projectImages/benefactory`,
+    false,
+    /.*\.png$/
+  ),
+  lighthouse: require.context(
+    `../../assets/images/projectImages/lighthouse`,
+    false,
+    /.*\.png$/
+  ),
+  nykaa: require.context(
+    `../../assets/images/projectImages/nykaa`,
+    false,
+    /.*\.png$/
+  ),
+  debGames: require.context(
+    `../../assets/images/projectImages/debGames`,
+    false,
+    /.*\.png$/
+  ),
+  vc_music_player: require.context(
+    `../../assets/images/projectImages/vc_music_player`,
+    false,
+    /.*\.png$/
+  ),
 };
 
-export const getProjectImages = id => {
+export const getProjectImages = (id) => {
   if (!id) return [];
 
   const images = projectImages[id];
@@ -30,9 +67,9 @@ export const getProjectImages = id => {
 // used to explicitly specify a image ratio to be show in grid
 export const imageSpecificRatio = {
   wakency: [{ id: "1", ratioWidth: 3, ratioHeight: 1 }],
-  benefactory: [{ id: '1', ratioWidth: 2, ratioHeight: 1 }],
+  benefactory: [{ id: "1", ratioWidth: 2, ratioHeight: 1 }],
   nykaa: [
-     {id: '1', ratioWidth: 3, ratioHeight: 1},
-     {id: '5', ratioWidth: 3, ratioHeight: 1},
-  ]
+    { id: "1", ratioWidth: 3, ratioHeight: 1 },
+    { id: "5", ratioWidth: 3, ratioHeight: 1 },
+  ],
 };
